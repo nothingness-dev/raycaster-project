5bd5ff163d928bcc56570e1cfd6cc3c506946190
nothingness-dev <nothingness.dev@gmail.com>
2026-01-29 21:31:22 +0330

    Adding game mode

--- map.h
+++ map.h
@@ -2,7 +2,22 @@
 #define MAP_H
 
 #include "raylib.h"
+#include "player.h" 
 
 void DrawSimpleMap(void);
+void DrawMapGrid(void);  
+void HandleMapEditor(Player *player);
+void DrawMousePositionInfo(void);
+void DrawGameStateInfo(void);
+
+typedef enum {
+    MODE_PLAY,
+    MODE_EDIT
+} GameMode;
+
+GameMode GetCurrentMode(void);
+void SetCurrentMode(GameMode mode);
+int GetShowMinimap(void);
+void ToggleShowMinimap(void);
 
 #endif
